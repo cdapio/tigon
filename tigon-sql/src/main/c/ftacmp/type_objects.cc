b2a0e6e16aa99a79ded5cf116f33a58589f5f1d4
johnsont <ted>
2014-10-08 08:53:21 -0400

    modify comment

--- tigon-sql/src/main/c/ftacmp/type_objects.cc
+++ tigon-sql/src/main/c/ftacmp/type_objects.cc
@@ -596,7 +596,7 @@ temporal_type data_type::compute_temporal_type(temporal_type l_tempo, temporal_t
   }
 
 //		If the value might be negative, can't deduce anything
-//                      However Java doesn't have unsigned types so the logic which forbids int has to be removed.
+//                      However Java doesn't have unsigned types so the logic which forbids int has to be relaxed.
   if(op == "*"){
 //  	if((l_type!=int_t && l_type!=floating_t && l_type!=llong_t) && (r_type!=int_t && r_type!=floating_t && r_type!=llong_t)){
         if(!((l_type==int_t || l_type==floating_t || l_type==llong_t) && (r_type==int_t || r_type==floating_t || r_type==llong_t))){
